3115ad2a84c56a6f36d4eb5f6e18698ff085449a
geneaky <kaktus418@gmail.com>
2022-07-10 19:49:48 +0900

    encapsulate variable

--- src/main/java/me/whiteship/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
+++ src/main/java/me/whiteship/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
@@ -1,13 +1,42 @@
 package me.whiteship.refactoring._05_global_data._17_encapsulate_variable;
 
 public class Thermostats {
-
     public static Integer targetTemperature = 70;
 
     public static Boolean heating = true;
 
     public static Boolean cooling = false;
-
     public static Boolean readInFahrenheit = true;
 
+    public static void setTargetTemperature(Integer targetTemperature) {
+        Thermostats.targetTemperature = targetTemperature;
+    }
+
+    public static void setHeating(Boolean heating) {
+        Thermostats.heating = heating;
+    }
+
+    public static void setCooling(Boolean cooling) {
+        Thermostats.cooling = cooling;
+    }
+
+    public static void setReadInFahrenheit(Boolean readInFahrenheit) {
+        Thermostats.readInFahrenheit = readInFahrenheit;
+    }
+
+    public static Integer getTargetTemperature() {
+        return targetTemperature;
+    }
+
+    public static Boolean getHeating() {
+        return heating;
+    }
+
+    public static Boolean getCooling() {
+        return cooling;
+    }
+
+    public static Boolean getReadInFahrenheit() {
+        return readInFahrenheit;
+    }
 }
