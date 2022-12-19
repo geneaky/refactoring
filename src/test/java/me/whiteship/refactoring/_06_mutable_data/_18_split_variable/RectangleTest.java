0018206f972c8f9d498b5712104edbf23b7d2169
geneaky <kaktus418@gmail.com>
2022-12-20 00:45:06 +0900

    mutate variable

--- src/test/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/RectangleTest.java
+++ src/test/java/me/whiteship/refactoring/_06_mutable_data/_18_split_variable/RectangleTest.java
@@ -1,8 +1,8 @@
 package me.whiteship.refactoring._06_mutable_data._18_split_variable;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class RectangleTest {
 
